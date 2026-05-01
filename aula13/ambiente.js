c2357191d397a47a238ba5c8b1fec1fadea00445
Eber Urzeda dos Santos <eber.urzeda@gmail.com>
2026-05-01 11:50:03 +0200

    Aulas 13 e 14

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+//2º forma de fazer repetições
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c<=6)
+
+/* principal forma de fazer repetições
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+*/
+
+/* opção menos usada de fazer repetições
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')*/
