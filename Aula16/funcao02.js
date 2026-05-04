1bcfc3f227fdc4af90a02ddf4e707140a4b4c155
Eber Urzeda dos Santos <eber.urzeda@gmail.com>
2026-05-04 13:05:34 +0200

    criei o ex016

---
+++ Aula16/funcao02.js
@@ -0,0 +1,5 @@
+function soma(n1=0, n2=0) {
+    return n1 + n2
+}
+console.log(soma(7, 3))
+//console.log(soma(7)) se a chamada é de apenas um numero, o n valerá "indefinido" ou o valor que eu pedir, n=0, por exemplo.
